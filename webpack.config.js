b50150cfa2ef6d413d6bdf52a9d6d3e36713691a
Stuart Mees <stuartmees@gmail.com>
2019-06-20 15:16:45 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   devtool: 'source-maps',
   module: {
